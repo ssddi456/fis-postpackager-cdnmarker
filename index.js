af55f17c50fbfc316e83787e05c3f1d9f2e1828e
Torrent Lee <torrent88410@gmail.com>
2015-11-05 15:34:20 +0800

    fix package name error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var path = require('path');
 module.exports = function( ret, conf, settings, opt ) {
     // foreach res/pkg add cdn properties
     // for which has a cdn properties
@@ -13,9 +14,11 @@ module.exports = function( ret, conf, settings, opt ) {
         var uriToIdMap = {};
         var fileToPack = {};
         var packToFile = {};
+
         fis.util.map(ret.map.pkg, function (id, pkg) {
             uriToIdMap[pkg.uri] = id;
         });
+
         fis.util.map(ret.pkg, function (subpath, file) {
             var uri = file.getUrl(opt.hash, opt.domain);
             var id = uriToIdMap[uri];
@@ -28,6 +31,7 @@ module.exports = function( ret, conf, settings, opt ) {
                 };
             }
         });
+
         return {
             packToFile: packToFile,
             fileToPack: fileToPack
@@ -36,14 +40,24 @@ module.exports = function( ret, conf, settings, opt ) {
 
     var pack_map = getPackMap.apply(null, [].slice.call(arguments));
 
+
     function absolutePathToRelativePath ( file ) {
         var content = file.getContent();
-        var f_url = file.uri;
+        var f_url = path.dirname(file.uri || file.url);
 
         var relative_to_file =  function( url ) {
-            if( url[0] == '/' ){
-                return path.relative(f_url, url);
+            var quot = url[0];
+
+            if( quot == '\'' || quot == '\"' ){
+                url = url.slice(1,-1);
+            } else {
+                quot = '';
             }
+
+            if( url[0] == '/' ){
+                return quot + path.relative(f_url, url).replace(/\\/g,'/') + quot;
+            }
+
             return url;
         }
 
@@ -69,19 +83,24 @@ module.exports = function( ret, conf, settings, opt ) {
             if( 'useCdn' in file ){
                 map_node.useCdn = file.useCdn ? 1 : 0;
             }
-        }
-
-        if( file.type == 'css'){
-            absolutePathToRelativePath(file);
+            if( map_node.type == 'css'){
+                absolutePathToRelativePath(file);
+            }
         }
     });
 
     fis.util.map( pkg, function( subpath, pkg ) {
+        var pkg_recode = pack_map.fileToPack[pkg.getId()];
+
+        if( !pkg_recode ){
+            return;
+        }
+
+        var pkg_recode = mpkg[pkg_recode.id];
         if( 'useCdn' in pkg ) {
-            var pkg_recode = mpkg[pack_map.fileToPack[pkg.getId()].id];
             pkg_recode.useCdn = pkg.useCdn ? 1 : 0;
         }
-        if( pkg.type == 'css'){
+        if( pkg_recode.type == 'css'){
             absolutePathToRelativePath(pkg);
         }
     });
